79ed525daba3bfed87a0fdec64b3053694e0bb16
Roman6753 <romanmironov056@gmail.com>
2025-10-29 16:47:53 +0300

    Install Laravel

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -58,7 +58,7 @@
 
     <footer class="bg-gray-800 text-white py-6 mt-12">
         <div class="container mx-auto px-4 text-center">
-            <p>&copy; 2025 Буквоежка. Портал обмена книгами.</p>
+            <p>Буквоежка. Портал обмена книгами.</p>
         </div>
     </footer>
 </body>
