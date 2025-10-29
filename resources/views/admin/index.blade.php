030369337964de0949519f13ba8c4723165222ed
Roman6753 <romanmironov056@gmail.com>
2025-10-29 18:29:48 +0300

    Install Laravel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -57,7 +57,6 @@
         @endif
     </div>
 
-    <!-- Модальное окно для указания причины отклонения -->
     <div id="rejectModal" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full hidden">
         <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
             <div class="mt-3">
